b3c02020c3777aae454023f9261b78ee971b0024
zhsl <angle555945@gmail.com>
2013-04-16 20:57:23 +0800

    Update Trie.cpp

--- string/Trie.cpp
+++ string/Trie.cpp
@@ -1,7 +1,39 @@
 /*  Trie   */
 
+/*  静态化,注意内存开辟大小    */
+struct Trie {
+    int ch[1<<16][10];    //内存开辟尽量最大
+    int val[1<<16];
+    int sz;
 
-/*  动态建立,释放内存要写递归函数  */
+    void init(){sz=1;mem(ch[0],0);}
+    void insert(char *s,int v){      //插入
+        int i,len=strlen(s),id,u=0;
+        for(i=0;i<len;i++){
+            id=s[i]-'0';
+            if(!ch[u][id]){
+                mem(ch[sz],0);
+                val[sz]=0;
+                ch[u][id]=sz++;
+            }
+            u=ch[u][id];
+        }
+        val[u]=v;
+    }
+    int find(char *s){  //查找
+        int i,len=strlen(s),id,u=0;
+        for(i=0;i<len;i++){
+            id=s[i]-'0';
+            if(!ch[u][id])return 0;
+            u=ch[u][id];
+        }
+        for(i=0;i<10;i++)
+            if(ch[u][id])return 1;   //存在prefix
+        return 2;  //存在
+    }
+}trie;
+
+/*  动态建立,注意释放内存  */
 struct Trie {
     struct Node{
         Node(){mem(ch,0);val=0;}
