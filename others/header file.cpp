a2f20c498b756c12afb0d5f6bec46a93aecce6c5
zhsl <angle555945@gmail.com>
2013-04-19 01:55:34 +0800

    Create header file.cpp

---
+++ others/header file.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<map>
+using namespace std;
+#define LL long long
+#define pii pair<int,int>
+#define mem(a,b) memset(a,b,sizeof(a))
+#define lson l,mid,rt<<1
+#define rson mid+1,r,rt<<1|1
+#define PI acos(-1.0)
+const int N=101010,INF=0x3f3f3f3f,MOD=10000,STA=8000010;
+const LL LNF=0x3f3f3f3f3f3f3f3f;
+const double DNF=1e13;
+//
+inline int Max(int a,int b){return a>b?a:b;}
+inline int Min(int a,int b){return a<b?a:b;}
+void swap(int& a,int& b){int t=a;a=b;b=t;}
+void swap(LL& a,LL& b){LL t=a;a=b;b=t;}
+//
+
+int main()
+{
+ //   freopen("in.txt","r",stdin);
+
+    return 0;
+}
