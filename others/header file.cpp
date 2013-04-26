a80c79ac3ed6a399c548032a783c17af79cc4fa1
zhsl <angle555945@gmail.com>
2013-04-26 10:21:36 +0800

    Update header file.cpp

--- others/header file.cpp
+++ others/header file.cpp
@@ -26,26 +26,17 @@ const int INF=0x3f3f3f3f;
 const int MOD=1000007,STA=8000010;
 const LL LNF=1LL<<60;
 const double EPS=1e-8;
-const double DNF=1e13;
 const double OO=1e15;
-
 //Daily Use ...
 template<class T> inline T Min(T a,T b){return a<b?a:b;}
 template<class T> inline T Max(T a,T b){return a>b?a:b;}
 template<class T> inline T Min(T a,T b,T c){return min(min(a, b),c);}
 template<class T> inline T Max(T a,T b,T c){return max(max(a, b),c);}
-template<class T> inline T Min(T a,T b,T c,T d){return min(min(a,b),min(c,d));}
-template<class T> inline T Max(T a,T b,T c,T d){return max(max(a,b),max(c,d));}
-void swap(int& a,int& b){int t=a;a=b;b=t;}
-void swap(LL& a,LL& b){LL t=a;a=b;b=t;}
-//
+//End
 
 int main()
 {
  //   freopen("in.txt","r",stdin);
-    LL a=-1235161423512355,b=123615412;
-    printf("%I64d\n",Min(a,b));
-
 
     return 0;
 }
