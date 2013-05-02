77551a26f3a7502ffefd3f4830613655ba410c59
zhsl <angle555945@gmail.com>
2013-05-02 18:51:37 +0800

    Update Trie.cpp

--- string/Trie.cpp
+++ string/Trie.cpp
@@ -2,15 +2,16 @@
 
 /*  静态化,注意内存开辟大小    */
 struct Trie {
-    int ch[1<<16][10];    //内存开辟尽量最大
-    int val[1<<16];
+    int ch[1<<13][26];    //内存开辟尽量最大
+    int val[1<<13];
     int sz;
 
     void init(){sz=1;mem(ch[0],0);}
+    int idx(char c){return c-'a';}
     void insert(char *s,int v){      //插入
         int i,len=strlen(s),id,u=0;
         for(i=0;i<len;i++){
-            id=s[i]-'0';
+            id=idx(s[i]);
             if(!ch[u][id]){
                 mem(ch[sz],0);
                 val[sz]=0;
@@ -23,11 +24,11 @@ struct Trie {
     int find(char *s){  //查找
         int i,len=strlen(s),id,u=0;
         for(i=0;i<len;i++){
-            id=s[i]-'0';
+            id=idx(s[i]);
             if(!ch[u][id])return 0;
             u=ch[u][id];
         }
-        for(i=0;i<10;i++)
+        for(i=0;i<26;i++)
             if(ch[u][id])return 1;   //存在prefix
         return 2;  //存在
     }
@@ -37,17 +38,18 @@ struct Trie {
 struct Trie {
     struct Node{
         Node(){mem(ch,0);val=0;}
-        Node *ch[10];   //根据情况开大小啦
+        Node *ch[26];   //根据情况开大小啦
         int val;
     };
     Node *head;
 
     void init(){head=new Node;}
+    int idx(char c){return c-'a';}
     void insert(char *s,int v){   //插入
         int i,len=strlen(s),id;
         Node *p=head,*q;
         for(i=0;i<len;i++){
-            id=s[i]-'0';   //求对应编号
+            id=idx(s[i]);   //求对应编号
             if(!p->ch[id]){
                 q=new Node;
                 p->ch[id]=q;
@@ -60,17 +62,17 @@ struct Trie {
         int i,len=strlen(s),id;
         Node *p=head;
         for(i=0;i<len;i++){
-            id=s[i]-'0';
+            id=idx(s[i]);
             if(!p->ch[id])return 0;
             p=p->ch[id];
         }
-        for(i=0;i<10;i++)
+        for(i=0;i<26;i++)
             if(p->ch[i])return 1;   //存在prefix
         return 2; //存在
     }
     void free(Node *p){   //释放内存
         int i;
-        for(i=0;i<10;i++)
+        for(i=0;i<26;i++)
             if(p->ch[i])free(p->ch[i]);
         delete p;
     }
