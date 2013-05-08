17baf9b56c42f0687b9830e056bf3e86462480d1
zhsl <angle555945@gmail.com>
2013-05-08 14:48:43 +0800

    Update gauss_elimination.cpp

--- math/gauss_elimination.cpp
+++ math/gauss_elimination.cpp
@@ -1,25 +1,32 @@
-/*   gauss_elimination   */
-
+/*   gauss_elimination  O(n^2) 
+   要求系数矩阵可逆
+   A[][]是增广矩阵,即A[i][n]是第i个方程右边的常数bi
+   运行结束后A[i][n]是第i个未知数的值    */
+   
 double A[N][N];
 
-void gauss(int A[][],int n)
+void gauss(int n)
 {
     int i,j,k,r;
     for(i=0;i<n;i++){
+        //选一行与r与第i行交换，提高数据值的稳定性
         r=i;
         for(j=i+1;j<n;j++)
-            if(fabs(A[j][i]) > fabs(A[r][i]) )r=j;
+            if(fabs(A[j][i]) > fabs(A[r][i]))r=j;
         if(r!=i)for(j=0;j<=n;j++)swap(A[r][j],A[i][j]);
-
-        for(k=i+1;k<n;k++){
+        //i行与i+1~n行消元
+      /*  for(k=i+1;k<n;k++){   //从小到大消元，中间变量f会有损失
             double f=A[k][i]/A[i][i];
             for(j=i;j<=n;j++)A[k][j]-=f*A[i][j];
-        }
-      /*  for(j=n;j>=i;j--){
+        }*/
+        for(j=n;j>=i;j--){   //从大到小消元，精度更高
             for(k=i+1;k<n;k++)
                 A[k][j]-=A[k][i]/A[i][i]*A[i][j];
-        }*/
+        }
+
+       // printf("%.30lf\n",A[i][i]);
     }
+    //回带过程
     for(i=n-1;i>=0;i--){
         for(j=i+1;j<n;j++)
             A[i][n]-=A[j][n]*A[i][j];
