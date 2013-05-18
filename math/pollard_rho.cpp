389284db453415d12c33f0578e41583718129703
zhsl <angle555945@gmail.com>
2013-05-18 18:40:47 +0800

    Create pollard_rho.cpp

---
+++ math/pollard_rho.cpp
@@ -0,0 +1,47 @@
+/*   pollard rho    
+  pollard_rho 算法进行质因数分解  */
+  
+LL factor[100];   //质因数分解结果（刚返回时是无序的）
+int tol;   //质因数的个数。数组小标从0开始
+
+LL gcd(LL a,LL b)
+{
+    if(a==0)return 1;
+    if(a<0) return gcd(-a,b);
+    while(b)
+    {
+        LL t=a%b;
+        a=b;
+        b=t;
+    }
+    return a;
+}
+
+LL Pollard_rho(LL x,LL c)
+{
+    LL i=1,k=2;
+    LL x0=rand()%x;
+    LL y=x0;
+    while(1)
+    {
+        i++;
+        x0=(mult_mod(x0,x0,x)+c)%x;
+        LL d=gcd(y-x0,x);
+        if(d!=1&&d!=x) return d;
+        if(y==x0) return x;
+        if(i==k){y=x0;k+=k;}
+    }
+}
+//对n进行素因子分解
+void findfac(LL n)
+{
+    if(Miller_Rabin(n))//素数
+    {
+        factor[tol++]=n;
+        return;
+    }
+    LL p=n;
+    while(p>=n)p=Pollard_rho(p,rand()%(n-1)+1);
+    findfac(p);
+    findfac(n/p);
+}
