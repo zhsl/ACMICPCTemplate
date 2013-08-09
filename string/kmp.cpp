073ba50404ae7eaf9a8329b6a1d09c91447b57e2
zhsl <angle555945@gmail.com>
2013-08-10 01:15:07 +0800

    Update kmp.cpp

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -1,14 +1,19 @@
-/*  KMP  */
+*  KMP  
+
+        0 1 2 3 4 5 6 7
+ s：    a b a b a b c d
+ next: -1 0 1 2 3 4 0 0    */
+
 
 int next[N];
 int T,n,m,len;
  
-void getnext(char *a,int len)
+void getnext(char *s,int len)
 {
     int j=0,k=-1;
     next[0]=-1;
     while(j<len){
-        if(k==-1 || a[k]==a[j])
+        if(k==-1 || s[k]==s[j])
             next[++j]=++k;
         else k=next[k];
     }
